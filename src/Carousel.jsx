0ce30732c3054842c772f00262fc90badacd633f
lucas <lucasportellaagu@gmail.com>
2022-03-11 22:34:51 -0300

    carousel linter

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -1,16 +1,14 @@
 import React, { useState, useEffect } from 'react';
 
 function Carousel() {
-  const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+  const alphabet = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const [spinners, setSpinners] = useState([0, 1, 2]);
 
   useEffect(() => {
     const carouselInterval = setInterval(() => {
       const spinnersCopy = JSON.parse(JSON.stringify(spinners));
-      console.log(spinnersCopy);
       const spinnersUpdated = spinnersCopy
         .map((spinner) => (spinner === alphabet.length - 1 ? 0 : spinner + 1));
-      console.log(spinnersUpdated);
       setSpinners(spinnersUpdated);
     }, 3000);
     return () => clearInterval(carouselInterval);
