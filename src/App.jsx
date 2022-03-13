142d50b15a2cde8c1f3140979986eb2347fda1eb
lucas <lucasportellaagu@gmail.com>
2022-03-13 17:20:08 -0300

    react router working

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import CarouselsWrapper from './Carousels/CarouselsWrapper';
+import NotRoot from './RoutesTests/NotRoot';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<CarouselsWrapper />} />
+        <Route path="notroot" element={<NotRoot />} />
       </Routes>
     </div>
   );
---
+++ src/RoutesTests/NotRoot.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function NotRoot() {
+  return <div>This is not the root page.</div>;
+}
+
+export default NotRoot;
