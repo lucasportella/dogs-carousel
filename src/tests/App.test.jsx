9181b129aa54108f1303c35b6b3a21f06bd35def
lucas <lucasportellaagu@gmail.com>
2022-03-17 12:16:12 -0300

    test if all images correctly loads asynchronously

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -4,10 +4,12 @@ import { screen } from '@testing-library/react';
 import RenderWithRouter from './RenderWithRouter';
 import App from '../App';
 
-test('renders learn react link', () => {
+test('renders learn react link', async () => {
   RenderWithRouter(<App />);
   const rawCarouselIntroTrext = screen.getByText(/Pure logical html carousel:/i);
   const dogCarouselLoadingText = screen.getByText(/Loading/i);
   expect(rawCarouselIntroTrext).toBeInTheDocument();
   expect(dogCarouselLoadingText).toBeInTheDocument();
+  const allImages = await screen.findAllByRole('img');
+  expect(allImages.length).toBe(6);
 });
